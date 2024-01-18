b323eb0f3c9f3acee36431d566a82c13fe9c4adc
Xandra <xandra.macatangay@gmail.com>
2024-01-18 17:34:09 +1100

    fixed close btn and scrollbar

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,20 @@
 import styles from "./Modal.module.scss"
 
 const Modal = ({toggleModal, description, publishedDate, pages, title }) => {
-
+  ////TOOK onClick={toggleModal} OFF OVERLAY!!!
   return (
     <div className={styles.modal}>
-      <div className={styles.overlay} onClick={toggleModal}></div>
+      <div className={styles.overlay}></div>
       <div className={styles.modal_content}>
         <div className={styles.space_between}>
-          <h4>{title}</h4>
-          
+        <h4>{title}</h4>
+        <button className={styles.close_modal} onClick={toggleModal}>Close</button>
         </div>
         
         <p><b>Pages: </b>{pages}</p>
         <p><b>Published: </b>{publishedDate}</p>
         <p><b>Description: </b>{description ? description : "No description found"}</p>
-        <button className={styles.close_modal} onClick={toggleModal}>Close</button>
+        
       </div>
     </div>
   )
